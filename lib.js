52f9db9470ea81b69efc5310e9a8db58de96b3de
Ahmed Elswerkey <khawater11212@gmail.com>
2019-01-24 11:48:17 +0200

    v1 of the simple js lib

---
+++ lib.js
@@ -0,0 +1,11 @@
+function $i(m){
+    if(document.getElementById(m) != undefined)
+        return document.getElementById(m);
+}
+function $c(m){
+    if(document.getElementsByClassName(m) != undefined)
+        return document.getElementsByClassName(m);
+}
+function cl(m){
+    console.log(m);
+} 
